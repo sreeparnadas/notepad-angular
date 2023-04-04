143a22da1745a41c9104d72397b32d24db271111
sreeparnadas <sreeparnadas0675@gmail.com>
2023-04-05 00:20:05 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import { NewListComponent } from './new-list/new-list.component';
+import { HeaderComponent } from './header/header.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
